a0af3c1743a413dba7861f2b72dfe533fc178830
000hcl <mnrjohansson@gmail.com>
2025-12-23 18:23:04 +0200

    ex 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,7 +3,23 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+
+morgan.token('body', (req) => {
+  return JSON.stringify(req.body)
+}
+)
+
+app.use(morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens.body(req)
+  ].join(' ')
+}))
 
 let persons = [
     { 
@@ -53,7 +69,6 @@ app.delete('/api/persons/:id', (request, response) => {
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
-  console.log(body);
 
   const person = {
     "id": Math.floor(Math.random()*1000000),
