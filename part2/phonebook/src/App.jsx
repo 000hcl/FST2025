52010e5e1c673d35b57144e22894b4c0ed2c9fe4
000hcl <mnrjohansson@gmail.com>
2025-12-26 21:40:29 +0200

    ex 3.20

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -68,12 +68,19 @@ const App = () => {
           setNotification(null)
         }, 5000)
       })
-      .catch(() => {
-          setError(`Information on ${newName} has already been removed from server.`)
+      .catch((error) => {
+          const msg = error.response.data.error
+          console.log(msg);
+          if (msg) {
+            setError(msg)
+          } else {
+            setError(`Information on ${newName} has already been removed from server.`)
+            setPersons(persons.filter(p => p.id !== person.id))
+          }
           setTimeout(()=>{
             setError(null)
           }, 5000)
-          setPersons(persons.filter(p => p.id !== person.id))
+          
       })
   }
 
