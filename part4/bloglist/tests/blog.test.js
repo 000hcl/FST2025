ba90645a9121312676804daa0e660f7137aa492e
000hcl <mnrjohansson@gmail.com>
2026-01-22 15:14:22 +0200

    ex 4.6

--- part4/bloglist/tests/blog.test.js
+++ part4/bloglist/tests/blog.test.js
@@ -94,3 +94,49 @@ describe('favorite blog', () => {
         assert.strictEqual(result, null)
     })
 })
+
+describe('most blogs', () => {
+    const blog1 =
+    {
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 500,
+    }
+    const blog2 = 
+    {
+        title: "On the Veg",
+        author: "Maverick Beeson",
+        url: "someurl.url",
+        likes: 500
+    }
+    const blog3 =
+    {
+        title: "Pizza Pies",
+        author: "John DiGiorno",
+        url: "pizzaurl.url",
+        likes: 490
+    }
+    const blog4 =
+    {
+        title: "Top 100 Vegetables",
+        author: "Maverick Beeson",
+        url: "someurl.url",
+        likes: 753
+    }
+
+    test('returns correct entry with longer list', () => {
+        const result = listHelper.mostBlogs([blog1, blog2, blog3, blog4])
+        assert.deepStrictEqual(result, {'author':'Maverick Beeson', 'blogs':2})
+    })
+
+    test('returns one entry with a list of one', () => {
+        const result = listHelper.mostBlogs([blog1])
+        assert.deepStrictEqual(result, {'author': 'Edsger W. Dijkstra', 'blogs':1})
+    })
+
+    test('returns undefined with empty list', () => {
+        const result = listHelper.mostBlogs([])
+        assert.strictEqual(undefined, result)
+    })
+})
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+const lodash = require('lodash')
+
 const dummy = (blogs) => {
     return 1
 }
@@ -17,8 +19,21 @@ const favoriteBlog = (blogs) => {
     return blogs.reduce((blog, favorite) => favorite.likes > blog.likes ? favorite : blog)
 }
 
+const mostBlogs = (blogs) => {
+    const mapper = (pair) => {
+        return {'author': pair[0], 'blogs': pair[1]}
+    }
+    const authorsCounted = lodash.countBy(blogs, (blog) => blog.author)
+    const pairs = lodash.toPairs(authorsCounted)
+    const mapped = lodash.map(pairs, mapper)
+    return lodash.maxBy(mapped, 'blogs')
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs
 }
+
+// notes: lodash partition, zipWith
