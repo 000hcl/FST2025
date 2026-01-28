0095f1cedbc4a5a918b28e3a4919a4ce5e2cff6d
000hcl <mnrjohansson@gmail.com>
2026-01-28 16:12:37 +0200

    ex 4.15

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -2,6 +2,7 @@ const config = require('./utils/config')
 const express = require('express')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const userRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 
 const app = express()
@@ -11,6 +12,7 @@ const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { family: 4 })
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', userRouter)
 app.use(middleware.errorHandler)
 
 module.exports = app
