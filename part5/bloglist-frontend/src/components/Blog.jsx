b5dc9644d371a605f863e0a846b66f221c4e4c88
000hcl <mnrjohansson@gmail.com>
2026-02-13 15:25:30 +0200

    ex 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false)
+
+  const toggleExpanded = () => {
+    setExpanded(!expanded)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return(
+    <div style={blogStyle}>
+      <div>
+      {blog.title} {blog.author} <button onClick={toggleExpanded}>{ expanded ? 'hide' :'view'}</button>
+      </div>
+      {expanded &&
+      <div>
+        {blog.url}
+        <br/>
+        {blog.likes} likes <button>like</button>
+        <br/>
+        {blog.user.name}
+      </div>
+      }
+    </div>
+    
+  )
+  
+}
 
 export default Blog
