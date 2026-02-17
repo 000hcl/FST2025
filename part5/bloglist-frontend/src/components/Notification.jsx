03e7c68e234a2a4226d960a782e8815fc722d961
000hcl <mnrjohansson@gmail.com>
2026-02-17 14:15:15 +0200

    ex 5.16, lint

--- part5/bloglist-frontend/src/components/Notification.jsx
+++ part5/bloglist-frontend/src/components/Notification.jsx
@@ -1,13 +1,13 @@
 const Notification = ({ message, classname }) => {
-    if (message === null) {
-      return null
-    }
-  
-    return (
-      <div className={classname}>
-        {message}
-      </div>
-    )
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className={classname}>
+      {message}
+    </div>
+  )
 }
-  
+
 export default Notification
--- part5/bloglist-frontend/src/services/login.js
+++ part5/bloglist-frontend/src/services/login.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 const baseUrl = '/api/login'
 
 const logIn = async (username, password) => {
-    const credentials = {username, password}
-    const response = await axios.post(baseUrl, credentials)
-    return response.data
+  const credentials = { username, password }
+  const response = await axios.post(baseUrl, credentials)
+  return response.data
 }
 
 export default { logIn }
