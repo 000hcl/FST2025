f57ae960e27cb08f7ab3f78e9eba0b6ebbfb390a
000hcl <mnrjohansson@gmail.com>
2026-03-17 14:02:04 +0200

    ex 8.10

---
+++ part8/library-frontend/src/components/NewBook.jsx
@@ -0,0 +1,106 @@
+import { useState } from 'react'
+import { gql } from '@apollo/client'
+import { useMutation } from '@apollo/client/react'
+
+const ADD_BOOK = gql`
+  mutation addBook(
+    $title: String!
+    $published: Int!
+    $author: String!
+    $genres: [String!]!
+  ) {
+    addBook(title: $title, published: $published, author: $author, genres: $genres) {
+      id
+      title
+      published
+      author
+      genres
+    }
+  }
+
+`
+const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      id
+      author
+      title
+      published
+    }
+  }
+`
+
+const NewBook = (props) => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [published, setPublished] = useState('')
+  const [genre, setGenre] = useState('')
+  const [genres, setGenres] = useState([])
+
+  const [addBook] = useMutation(ADD_BOOK, {
+    refetchQueries: [{ query: ALL_BOOKS }]
+  })
+
+  if (!props.show) {
+    return null
+  }
+
+  const submit = async (event) => {
+    event.preventDefault()
+
+    addBook({ variables: { title, published: Number(published), author, genres } })
+
+    setTitle('')
+    setPublished('')
+    setAuthor('')
+    setGenres([])
+    setGenre('')
+  }
+
+  const addGenre = () => {
+    setGenres(genres.concat(genre))
+    setGenre('')
+  }
+
+  return (
+    <div>
+      <form onSubmit={submit}>
+        <div>
+          title
+          <input
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          author
+          <input
+            value={author}
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          published
+          <input
+            type="number"
+            value={published}
+            onChange={({ target }) => setPublished(target.value)}
+          />
+        </div>
+        <div>
+          <input
+            value={genre}
+            onChange={({ target }) => setGenre(target.value)}
+          />
+          <button onClick={addGenre} type="button">
+            add genre
+          </button>
+        </div>
+        <div>genres: {genres.join(' ')}</div>
+        <button type="submit">create book</button>
+      </form>
+    </div>
+  )
+}
+
+export default NewBook
