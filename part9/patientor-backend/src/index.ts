2765e60095913700bd6df1b943245ac1280c5376
000hcl <mnrjohansson@gmail.com>
2026-03-29 15:26:15 +0300

    ex 9.10

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import diagnosisRouter from './routes/diagnoses'
 const app = express();
 app.use(express.json());
 
@@ -9,6 +10,8 @@ app.get('/api/ping', (_req, res) => {
   
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosisRouter)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ part9/patientor-backend/src/services/diagnosisService.ts
@@ -0,0 +1,12 @@
+import diagnosisData from '../../data/diagnoses'
+import { Diagnosis } from '../types';
+
+const diagnoses: Diagnosis[] = diagnosisData as Diagnosis[]
+
+const getDiagnoses = () => {
+    return diagnoses;
+};
+
+export default {
+    getDiagnoses
+};
