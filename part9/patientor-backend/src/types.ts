b1911a327fd5c662646143016184bd5ab3368b48
000hcl <mnrjohansson@gmail.com>
2026-03-29 21:24:55 +0300

    ex 9.11

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -3,4 +3,12 @@ export interface Diagnosis {
     name: string;
     latin?: string;
 
-}
+};
+
+export interface Patient {
+    id: string;
+    name: string;
+    dateOfBirth: string;
+    gender: string;
+    occupation: string;
+};
