54ed49cce5a8b76f04f3c09373f2694e08aead2f
000hcl <mnrjohansson@gmail.com>
2026-04-01 15:34:43 +0300

    ex 9.21

--- part9/patientor-backend/src/services/patientService.ts
+++ part9/patientor-backend/src/services/patientService.ts
@@ -1,9 +1,9 @@
 import { v1 as uuid } from 'uuid';
 
 import patientData from '../../data/patients';
-import { NewPatient, Patient, Gender, PatientAll } from '../types';
+import { NewPatient, Patient, Gender, PatientAll, NonSensitivePatient } from '../types';
 
-const getPatients = (): Patient[] => {
+const getPatients = (): NonSensitivePatient[] => {
     return patientData.map(({ id, name, dateOfBirth, gender, occupation }) => ({
         id,
         name,
@@ -13,32 +13,31 @@ const getPatients = (): Patient[] => {
     }));
 };
 
-const addPatient = (obj: NewPatient): Patient => {
-    const patient: PatientAll = {...obj, id: uuid()};
+const findById = (id: string): Patient | undefined => {
+    const patientInfo = patientData.find((p) => p.id === id);
+    if (patientInfo) {
+        const patient = {
+            id: patientInfo.id,
+            name: patientInfo.name,
+            dateOfBirth: patientInfo.dateOfBirth,
+            gender: parseGender(patientInfo.gender),
+            occupation: patientInfo.occupation,
+            entries: [],
+            ssn: patientInfo.ssn
+        };
+        return patient;
+    } else {
+        return;
+    }
+};
+
+const addPatient = (obj: NewPatient): NonSensitivePatient => {
+    const patient: PatientAll = {...obj, id: uuid(), entries:undefined};
     patientData.push(patient);
     return censoredPatient(patient);
 };
 
-// const toNewPatient = (obj: unknown): NewPatient => {
-//     if ( !obj || typeof obj !== 'object' ) {
-//         throw new Error('Incorrect or missing data');
-//     }
-//     if ('name' in obj && 'dateOfBirth' in obj && 'gender' in obj && 'occupation' in obj && 'ssn' in obj) {
-//         const patient = {
-//             id: uuid(),
-//             name: parseString(obj.name),
-//             dateOfBirth: parseDate(obj.dateOfBirth),
-//             gender: parseGender(obj.gender),
-//             occupation: parseString(obj.occupation),
-//             ssn: parseString(obj.ssn)
-
-//         };
-//         return patient;
-//     }
-//     throw new Error('Incorrect data: some fields are missing');
-// };
-
-const censoredPatient = (obj: PatientAll): Patient => {
+const censoredPatient = (obj: Patient): NonSensitivePatient => {
     return {
         id: obj.id,
         name: obj.name,
@@ -56,12 +55,6 @@ const isString = (text: unknown): text is string => {
     return typeof text === 'string' || text instanceof String;
 };
 
-// const parseString = (text: unknown): string => {
-//     if (!text || !isString(text)) {
-//         throw new Error(`Incorrect or missing string: ${text}`);
-//     }
-//     return text;
-// };
 
 const parseGender = (gender: unknown): Gender => {
     if (!gender || !isString(gender) || !isGender(gender)) {
@@ -70,18 +63,8 @@ const parseGender = (gender: unknown): Gender => {
     return gender;
 };
 
-// const isDate = (date: string): boolean => {
-//     return Boolean(Date.parse(date));
-//   };
-  
-// const parseDate = (date: unknown): string => {
-// if (!date || !isString(date) || !isDate(date)) {
-//     throw new Error('Incorrect or missing date: ' + date);
-// }
-// return date;
-// };
-
 export default {
     getPatients,
-    addPatient
+    addPatient,
+    findById
 };
--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -8,13 +8,21 @@ export interface Diagnosis {
 
 };
 
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface Entry {
+}
+
 export interface Patient {
-    id: string;
-    name: string;
-    dateOfBirth: string;
-    gender: Gender;
-    occupation: string;
-};
+  id: string;
+  name: string;
+  ssn: string;
+  occupation: string;
+  gender: Gender;
+  dateOfBirth: string;
+  entries?: Entry[]}
+
+export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
 
 export type NewPatient = z.infer<typeof NewPatientSchema>;
 
