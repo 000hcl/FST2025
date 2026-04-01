2942a4b413449e4b6f52cfcca38e79098b81d2eb
000hcl <mnrjohansson@gmail.com>
2026-04-01 11:41:41 +0300

    ex 9.18

--- part9/flight-diary-frontend/src/App.tsx
+++ part9/flight-diary-frontend/src/App.tsx
@@ -6,16 +6,37 @@ interface DiaryEntry {
   date: string;
   weather: string;
   visibility: string;
-} 
+  comment: string;
+}
+type NewDiaryEntry = Omit<DiaryEntry, 'id'>
+
 
 const App = () => {
   const [entries, setEntries] = useState<DiaryEntry[]>([])
+  const [visibility, setVisibility] = useState<string>('')
+  const [weather, setWeather] = useState<string>('')
+  const [date, setDate] = useState<string>('')
+  const [comment, setComment] = useState<string>('')
 
   useEffect(() => {
     axios.get<DiaryEntry[]>('http://localhost:3000/api/diaries').then(response => {
       setEntries(response.data)
     })
   })
+  const createEntry = (event: React.SyntheticEvent) => {
+    event.preventDefault()
+    const newEntry:NewDiaryEntry = {
+      visibility,
+      weather,
+      date,
+      comment
+    }
+    axios.post<DiaryEntry>('http://localhost:3000/api/diaries', newEntry).then(
+      response => {
+        setEntries(entries.concat(response.data))
+      }
+    )
+  }
   return (
     <div>
       <h2>Diary entries</h2>
@@ -27,9 +48,23 @@ const App = () => {
             weather: {e.weather}
             <br/>
             visibility: {e.visibility}
+            <br/>
+            comment: {e.comment}
           </p>
         ))}
       </div>
+      <h2>add new</h2>
+        <form onSubmit={createEntry}>
+          date :<input value={date} onChange={(event) => setDate(event.target.value)}/>
+          <br/>
+          weather :<input value={weather} onChange={(event) => setWeather(event.target.value)}/>
+          <br/>
+          visibility :<input value={visibility} onChange={(event) => setVisibility(event.target.value)}/>
+          <br/>
+          comment :<input value={comment} onChange={(event) => setComment(event.target.value)}/>
+          <br/>
+          <button type="submit">submit</button>
+        </form>
     </div>
   );
 };
